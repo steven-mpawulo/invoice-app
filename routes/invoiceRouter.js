576a158121166fbd2072f190d9d1541e536a46a9
steven-mpawulo <stevenmpawulo@gmail.com>
2023-08-28 13:04:04 +0300

    adds retreives clients controller

--- routes/invoiceRouter.js
+++ routes/invoiceRouter.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const saveClient = require('../controllers/saveClient');
+const retreiveClients = require('../controllers/retreiveClients');
 const invoiceRouter = express.Router();
 
 
 
 invoiceRouter.post('/clients', saveClient);
+invoiceRouter.get('clients', retreiveClients);
 
 module.exports = invoiceRouter;
 
