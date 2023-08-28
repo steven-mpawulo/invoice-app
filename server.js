9e6281883386f3e271960a0fe04c2b9249ae0a2f
steven-mpawulo <stevenmpawulo@gmail.com>
2023-08-28 12:24:10 +0300

    connects to databse

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 require('dotenv').config();
+const mongoose = require('mongoose');
 
 const app = express();
 
@@ -9,6 +10,12 @@ app.get('/', (req, res) => {
 
 const port = process.env.PORT || 5001;
 
-app.listen(port, () => {
-    console.log("server started");
-});
+mongoose.connect(process.env.DB_URL).then(() => {
+    console.log("connected to database");
+    app.listen(port, () => {
+        console.log("server started");
+    });
+}).catch((e) => {
+    console.log(e);
+    console.log("failed to connect to database");
+})
