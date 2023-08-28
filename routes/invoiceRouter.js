d29f9176a6423770ff912f6b817b0c08a169a376
steven-mpawulo <stevenmpawulo@gmail.com>
2023-08-28 15:40:16 +0300

    adds delete invoice controller

--- routes/invoiceRouter.js
+++ routes/invoiceRouter.js
@@ -2,6 +2,7 @@ const express = require('express');
 const saveClient = require('../controllers/saveClient');
 const retreiveClients = require('../controllers/retreiveClients');
 const createInvoice = require('../controllers/createInvoice');
+const deleteInvoice = require('../controllers/deleteInvoice');
 const invoiceRouter = express.Router();
 
 
@@ -9,6 +10,7 @@ const invoiceRouter = express.Router();
 invoiceRouter.post('/clients', saveClient);
 invoiceRouter.get('/clients', retreiveClients);
 invoiceRouter.post('/invoices/:id', createInvoice);
+invoiceRouter.delete('invoice/:id', deleteInvoice);
 
 module.exports = invoiceRouter;
 
