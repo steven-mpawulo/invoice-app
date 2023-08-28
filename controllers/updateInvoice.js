64117a36f85524abf5e47a21b72ebe6980bd1a48
steven-mpawulo <stevenmpawulo@gmail.com>
2023-08-28 16:57:00 +0300

    works on updating invoice values

--- controllers/updateInvoice.js
+++ controllers/updateInvoice.js
@@ -7,7 +7,7 @@ const updateInvoice = async (req, res) => {
 
     if (Object.keys(body).length !== 0) {
         if (invoiceId !== null) {
-            await Invoice.findByIdAndUpdate({ '_id': invoiceId }, { $set: { "total": body.total } }, { $addToSet: { "items": body.items, "hours": body.hours, "rates": body.rates } }, {new: true}).then((value) => {
+            await Invoice.findByIdAndUpdate({ '_id': invoiceId }, { $set: { "total": body.total, "items": body.items, "hours": body.hours, "rates": body.rates } } , {new: true}).then((value) => {
                 console.log(value);
                 res.status(200).json({"message": "invoice updated", "invoice": value});
             }).catch((e) => {
