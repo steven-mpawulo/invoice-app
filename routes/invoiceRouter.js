9000f61071473ff49f0b2df2f0221ad09e56ba4b
steven-mpawulo <stevenmpawulo@gmail.com>
2023-08-29 09:42:49 +0300

    adds updating client information controller

--- routes/invoiceRouter.js
+++ routes/invoiceRouter.js
@@ -5,12 +5,14 @@ const createInvoice = require('../controllers/createInvoice');
 const deleteInvoice = require('../controllers/deleteInvoice');
 const updateInvoice = require('../controllers/updateInvoice');
 const getInvoices = require('../controllers/getInvoices');
+const updateClient = require('../controllers/updateClient');
 const invoiceRouter = express.Router();
 
 
 
 invoiceRouter.post('/clients', saveClient);
 invoiceRouter.get('/clients', retreiveClients);
+invoiceRouter.put('/clients/:id', updateClient);
 invoiceRouter.post('/invoices/:id', createInvoice);
 invoiceRouter.delete('/invoices/:id', deleteInvoice);
 invoiceRouter.put('/invoices/:id', updateInvoice);
