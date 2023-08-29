6ec6ad1e1e3c808057d35b0bcc84853f60da4895
steven-mpawulo <stevenmpawulo@gmail.com>
2023-08-29 09:40:29 +0300

    creates update client controller

---
+++ controllers/updateClient.js
@@ -0,0 +1,28 @@
+const Client = require("../models/Client");
+
+const updateClient = async (req, res) => {
+    const body = req.body;
+    console.log(body);
+    const clientId = req.params.id;
+    console.log(clientId);
+
+    if (Object.keys(body).length !== 0){
+        if (body.fullName && body.email && body.phoneNumber) {
+            await Client.findByIdAndUpdate({'_id': clientId}, {$set: {"fullName": body.fullName, "email": body.email, "phoneNumber": body.phoneNumber} }).then((value) => {
+                console.log(value);
+                res.status(201).json({"message": "client updated", "client": value});
+            }).catch((e) => {
+                console.log(e);
+                res.status(400).json({"message": "failed to update client"});
+            });
+        } else {
+            res.status(400).json({"message": "pleaase provide some data"});
+        }
+
+    } else {
+        res.status(400).json({"message": "please provide some datat"});
+    }
+}
+
+
+module.exports = updateClient;
